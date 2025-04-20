9db130d0bfc17a1cbb9b598ac9d03590014637ee
DavidSoi96 <david.soi@student.moringaschool.com>
2025-04-20 14:53:03 +0300

    assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, is_anagrams):
+        matches = []
+        for the_word in is_anagrams:
+            the_word_lower = the_word.lower()
+            if the_word_lower != self.word  and sorted(the_word_lower) == self.sorted_word:
+                matches.append(the_word)
+        return matches 
